c81909985f514f56a4ca42b76817074bffdd1bc7
Kevin <52282940+WuShaoa@users.noreply.github.com>
2025-02-26 20:33:59 +0800

    add source files

--- TS_NG/src/tsdatasets.py
+++ TS_NG/src/tsdatasets.py
@@ -82,6 +82,120 @@ class Addr1394Dataset(TimeseriesDataset):
                 masked_data.loc[pos] = np.random.randint(0, 255)
             # else 10% keep the same
         return masked_data, masked_tok
+    def get_seg_swap(self, tc):
+        disturb_scale = DISTURB_SCALE #255 #0.25 #<arg>
+        disturb_probability = DISTURB_PROBABILITY #0.05 0.02 #<arg> 0.01
+        disturb_n_threshold_min = DISTURB_N_THRESHOLD_MIN #0.2 <arg>
+        disturb_n_threshold_max = DISTURB_N_THRESHOLD_MAX #0.8 #<arg>
+        error_split_probablity = ERROR_SPLIT_PROBABLITY #<arg>
+        # dd = 1
+        disturbc = []
+        labelsc = np.zeros_like(tc)
+        labelsc_nc = np.zeros_like(tc) #labels for n_classes
+        ttc = tc.copy()
+        np.random.seed(RANDOM_SEED) #<arg>
+        
+        # # abnormal: disturb
+        disturb_indices = []
+        # for i, _ in enumerate(tc):
+            
+        #     # if np.random.rand(1)[0] < disturb_probability: # !change thedistrubution of disturb errors' choices here
+        #     #DISTURB_CENTERED
+        #     if i in [10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,2930,31,32,33,34,35,36,37,38,39,40,
+        #              510,511,512,513,514,515,516,517,518,519,520,521,522,523,524525,526,527,528,529,530,
+        #              610,611,612,613,614,615,616,617,618,619,620,621,622,623,624625,626,627,628,629,630, 
+        #              710,711,712,713,714,715,716,717,718,719,720,721,722,723,724725,726,727,728,729,730,    
+        #              100,101,102,103,104,500,501,502,503,504,505,506,800,801,802803]: # !change the distrubution of disturb errors' choices here
+        #     # if np.random.randn(1)[0] > 2.1: # disturb_probability:
+        #         # if np.random.rand(1)[0] < error_split_probablity or i == 0 or i== len(tc)-1:
+        #         d = disturb_scale * np.random.random(1)[0]
+        #         while abs(d) < disturb_n_threshold_min or abs(d) >disturb_n_threshold_max or abs(d) == ttc[:,0][i]:
+        #             d = disturb_scale * np.random.random(1)[0] #disturb_scale *np.abs(np.random.randn(1)[0]) + 0.3 #(np.random.rand(1)[0] + 0.5) * 2 *disturb_scale
+        #         ttc[i] += d  
+        #         ttc[i] = abs(ttc[i])
+
+        #         # d = disturb_scale * np.random.randn(1)[0]
+        #         # while d < disturb_n_threshold_min or d >disturb_n_threshold_max or d == ttc[i]:
+        #         #     d = disturb_scale * np.random.randn(1)[0]
+        #         # ttc[i] = d    
+        #         # labelsc.append(np.array([1.0, 0.0])) #1 # abnormal
+        #         # print(ttc[i])
+        #         labelsc[i] = 1 #False #1 # abnormal [-TODO](+DONE:shift USAD andTrainAD output)：comment for test
+        #         labelsc_nc[i] = 1 # n_classes
+        #         disturb_indices.append(i)
+        #         disturbc.append(d)
+        #         # else:
+        #         #     # swap error
+        #         #     temp = tc[i + 1]
+        #         #     tc[i + 1] = tc[i]
+        #         #     tc[i] = temp
+        #         #     labelsc.append(np.array([1.0]))
+        #     else:
+        #         #labelsc.append(np.array([0.0])) #0#[-TODO](+DONE:shift USAD andTrainAD output)：comment for test
+        #         disturbc.append(0.0)#[-TODO](+DONE:shift USAD and TrainADoutput)：comment for test
+
+        # abnormal: exchange
+        
+        from_index_pairs = [(10, 10+50), (210, 210+50), (410, 410+50), (710, 71+50)]
+        to_index_pairs =   [(110, 110+50), (320, 320+50), (500, 500+50), (810, 81+50)]
+        for from_pair, to_pair in zip(from_index_pairs, to_index_pairs):
+            from_index = from_pair[0]
+            to_index = to_pair[0]
+            for i in range(from_pair[1] - from_pair[0]):
+                temp = ttc[from_index + i].copy()
+                ttc[from_index + i] = tc[to_index + i]
+                tc[to_index + i] = temp
+                labelsc[from_index + i] = 1
+                labelsc_nc[from_index + i] = 1
+                labelsc[to_index + i] = 1
+                labelsc_nc[to_index + i] = 2
+                disturb_indices.append(from_index + i)
+                disturbc.append(ttc[from_index + i] - tc[to_index + i])
+                disturb_indices.append(to_index + i)
+                disturbc.append(ttc[to_index + i] - tc[from_index + i])
+        return ttc, labelsc, labelsc_nc, disturb_indices, disturbc
+    def get_random_disturb(self, tc):
+        disturb_scale = DISTURB_SCALE #255 #0.25 #<arg>
+        disturb_probability = DISTURB_PROBABILITY #0.05 0.02 #<arg> 0.01
+        disturb_n_threshold_min = DISTURB_N_THRESHOLD_MIN #0.2 <arg>
+        disturb_n_threshold_max = DISTURB_N_THRESHOLD_MAX #0.8 #<arg>
+        error_split_probablity = ERROR_SPLIT_PROBABLITY #<arg>
+        # dd = 1
+        disturbc = []
+        labelsc = np.zeros_like(tc)
+        ttc = tc.copy()
+        np.random.seed(RANDOM_SEED) #<arg>
+
+        for i, _ in enumerate(tc):
+            # abnormal: disturb
+            if np.random.rand(1)[0] < disturb_probability:
+            # if np.random.randn(1)[0] > 2.1: # disturb_probability:
+                # if np.random.rand(1)[0] < error_split_probablity or i == 0 or i == len(tc)-1:
+                d = disturb_scale * np.random.random(1)[0]
+                while abs(d) < disturb_n_threshold_min or abs(d) > disturb_n_threshold_max or abs(d) == ttc[:,0][i]:
+                    d = disturb_scale * np.random.random(1)[0] #disturb_scale * np.abs(np.random.randn(1)[0]) + 0.3 #(np.random.rand(1)[0] + 0.5) * 2 * disturb_scale
+                ttc[:,0][i] += d  #TODO: disturb_scale
+                ttc[:,0][i] = abs(ttc[:,0][i])
+
+                # d = disturb_scale * np.random.randn(1)[0]
+                # while d < disturb_n_threshold_min or d > disturb_n_threshold_max or d == ttc[i]:
+                #     d = disturb_scale * np.random.randn(1)[0]
+                # ttc[i] = d
+                # labelsc.append(np.array([1.0, 0.0])) #1 # abnormal
+                # print(ttc[i])
+                labelsc[i,0] = 1 #False #1 # abnormal [-TODO](+DONE:shift USAD and TrainAD output)：comment for test
+                disturbc.append(d)
+                # else:
+                #     # swap error
+                #     temp = tc[i + 1]
+                #     tc[i + 1] = tc[i]
+                #     tc[i] = temp
+                #     labelsc.append(np.array([1.0]))
+            else:
+                #labelsc.append(np.array([0.0])) #0#[-TODO](+DONE:shift USAD and TrainAD output)：comment for test
+                disturbc.append(0.0)#[-TODO](+DONE:shift USAD and TrainAD output)：comment for test
+    
+        
 
 class WindowGenerator:
     def __init__(
